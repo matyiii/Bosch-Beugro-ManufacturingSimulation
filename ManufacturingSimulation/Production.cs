ce8445bbc753ffd1e152e6e20628f06942450882
matyiii <mezofimate@gmail.com>
2022-06-29 13:03:39 +0200

    Add project files.

---
+++ ManufacturingSimulation/Production.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManufacturingSimulation
+{
+    internal class Production
+    {
+        public int Id { get; set; }
+        public int PcbId { get; set; }
+
+        public string Quantity { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public Production(int id, int pcbId, string quantity, DateTime startDate, DateTime endDate)
+        {
+            Id = id;
+            PcbId = pcbId;
+            Quantity = quantity;
+            StartDate = startDate;
+            EndDate = endDate;  
+        }
+    }
+}
